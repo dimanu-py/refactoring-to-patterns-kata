1cb068584c92f4e38f6e89ef2c3321c6f62dd989
dimanu-py <diegomtz126@gmail.com>
2024-09-20 11:12:45 +0200

    refactor(compose-method): extract method _increase_size_by

--- src/refactoring_to_patterns/compose_method/list.py
+++ src/refactoring_to_patterns/compose_method/list.py
@@ -17,13 +17,16 @@ class CustomList:
             new_size = self._size + 1
 
             if new_size > len(self._elements):
-                new_elements = [None] * (len(self._elements) + 10)
-                for i in range(self._size):
-                    new_elements[i] = self._elements[i]
-                self._elements = new_elements
+                self._increase_size_by(10)
 
             self._elements[self._size] = element
             self._size += 1
+
+    def _increase_size_by(self, amount: int) -> None:
+        new_elements = [None] * (len(self._elements) + amount)
+        for i in range(self._size):
+            new_elements[i] = self._elements[i]
+        self._elements = new_elements
 
     def elements(self) -> list[Any]:
         return self._elements
